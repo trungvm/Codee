837fde3ae81f494f5f456599a4bc138e3ce09d96
Trung <37643391+trungvm@users.noreply.github.com>
2019-05-10 09:58:34 +0700

    Add files via upload

---
+++ 1.DetectObj/build/CMakeFiles/offboard.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/offboard.dir/offboardcontrol.cpp.o"
+  "CMakeFiles/offboard.dir/rsCam.cpp.o"
+  "offboard.pdb"
+  "offboard"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/offboard.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
